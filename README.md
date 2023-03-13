0c9e543a405db2d94317e98ee9933d748948d5cc
AjmalAhamedKodakkat <ajmalbinahamed@gmail.com>
2023-03-13 12:04:28 +0400

    Added LogicAML contract

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ contract MyContract {
 1. Import the Molecule Protocol Interface smart contract
 2. Cast the sanction list address and call to check the recipient
 3. You can pick a list of sanction list or deploy your own custom list in the molecule contract
+
+
+## Deployments  Goerli 
+
+- Molecule contract : [0x1B74ff3615C982872C150E6E237Ad303240031CC](https://goerli.etherscan.io/address/0x1b74ff3615c982872c150e6e237ad303240031cc)
+
+- LogicAML US (ID:840) contract address : [0xEB98D082006422C7E72b480FCe912fc9fb7D1938](https://goerli.etherscan.io/address/0xEB98D082006422C7E72b480FCe912fc9fb7D1938)
+
+- LogicAML UK (ID:826) contract address :[0xA31307ab146a5E8A593277911C276Ac078809FA4](https://goerli.etherscan.io/address/0xA31307ab146a5E8A593277911C276Ac078809FA4)
---
+++ src/LogidAML.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ILogicAddress.sol";
+
+/// @title Molecule Protocol LogicAML contract
+/// @dev This contract implements the ILogicAddress interface with address input
+///      It will return true if the `account` exists in the List
+contract LogicAML is Ownable, ILogicAddress {
+    constructor() {}
+
+    mapping(address => bool) private batchData;
+
+    event ListAdded(address[] addrs);
+    event ListRemoved(address[] addrs);
+
+    // To update the LogicAML list
+    function updateList(address[] memory _addAddress)
+        external
+        onlyOwner
+        returns (bool)
+    {
+        for (uint256 i = 0; i < _addAddress.length; i++) {
+            batchData[_addAddress[i]] = true;
+        }
+        emit ListAdded(_addAddress);
+        return true;
+    }
+
+    // Remove address from the List
+    function removeFromList(address[] memory _removeAddress)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _removeAddress.length; i++) {
+            batchData[_removeAddress[i]] = false;
+        }
+        emit ListRemoved(_removeAddress);
+    }
+
+    // checks whether the address is present inside the list and returns true if its present
+    function check(address account) external view returns (bool) {
+        return batchData[account];
+    }
+}
