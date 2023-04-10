d54c97995dc6d3727954e381c17d420a28222087
calvin <calvinpak@gmail.com>
2023-04-10 07:24:45 -0700

    initial checkin for v2 set of contracts

---
+++ v2/contracts/MoleculeLogicList.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IMoleculeLogic.sol";
+
+/// @title Molecule Protocol Logic List
+/// @dev This contract implements the ILogicAddress interface with address input
+///      It will return true if the `account` exists in the List
+abstract contract MoleculeLogicList is Ownable, IMoleculeLogic {
+    // Human readable name of the list
+    string public _name;
+    // True if the list is an allowlist, false if it is a Blocklist
+    bool public _isAllowlist;
+
+    mapping(address => bool) public _list;
+
+    event ListAdded(address[] addresses);
+    event ListRemoved(address[] addresses);
+    event LogicCreated(string name, bool isAllowlist);
+
+    constructor(string memory name_, bool isAllowlist_) {
+        // Name and the allowlist/blocklist can only be set during creation
+        _name = name_;
+        _isAllowlist = isAllowlist_;
+        emit LogicCreated(name_, isAllowlist_);
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function isAllowlist() external view returns (bool) {
+        return _isAllowlist;
+    }
+
+    // Returns true if the address is sanctioned
+    function check(address account) external view returns (bool) {
+        return _list[account];
+    }
+
+    // Owner only functions
+    // Add addresses to the List
+    function addBatch(address[] memory addresses) external onlyOwner returns (bool) {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _list[addresses[i]] = true;
+        }
+        emit ListAdded(addresses);
+        return true;
+    }
+
+    // Remove addresses from the List
+    function removeBatch(address[] memory addresses) external onlyOwner {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _list[addresses[i]] = false;
+        }
+        emit ListRemoved(addresses);
+    }
+}
---
+++ v2/contracts/MoleculeLogicNFT.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IMoleculeLogic.sol";
+
+/// @title Molecule Protocol Logic NFT-gating contract (ERC721 only)
+/// @dev This contract implements the ILogicAddress interface with address input
+///      It will return true if the `account` owns any NFTs in the specified contract
+///  Note: 1155 requires tokenId as input, currently not supported
+contract MoleculeLogicNFT is Ownable, IMoleculeLogic {
+    // Human readable name of the list
+    string public _name;
+    // True if the list is an allowlist, false if it is a Blocklist
+    bool public _isAllowlist;
+    // NFT contract address
+    address public _nftContract;
+
+    event NFTContractSet(address nftContract);
+    event NFTLogicCreated(string name, bool isAllowlist);
+
+    constructor(string memory name_, bool isAllowlist_, address nftContract_) {
+        // Name and the allowlist/blocklist can only be set during creation
+        _name = name_;
+        _isAllowlist = isAllowlist_;
+        emit NFTLogicCreated(name_, isAllowlist_);
+        setNFTContract(nftContract_);
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function isAllowlist() external view returns (bool) {
+        return _isAllowlist;
+    }
+
+    function nftContract() external view returns (address) {
+        return _nftContract;
+    }
+
+    // Returns true if the address has the NFT
+    function check(address account) external view override returns (bool) {
+        return IERC721(_nftContract).balanceOf(account) > 0;
+    }
+
+    // Owner only functions
+    // Set NFT contract address
+    function setNFTContract(address nftContract_) public onlyOwner {
+        _nftContract = nftContract_;
+        emit NFTContractSet(nftContract_);
+    }
+}
