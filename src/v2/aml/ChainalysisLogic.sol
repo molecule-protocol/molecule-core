1ff1d9364896c83778277a4c80acd23a3dd6a5cf
AjmalAhamedKodakkat <ajmalbinahamed@gmail.com>
2023-11-16 10:21:36 +0530

    updated chainalysis contract

--- src/v2/aml/ChainalysisLogic.sol
+++ src/v2/aml/ChainalysisLogic.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../MoleculeLogic.sol";
 
 // Chainalysis SanctionList template
@@ -11,14 +12,18 @@ interface SanctionsList {
 }
 
 // This contract is immutable, no way to change once deployed
-contract AML is MoleculeLogic {
+contract AML is MoleculeLogic,Ownable {
     // Chainalysis SanctionList contract address
-    address public immutable sanctionList = 0x40C57923924B5c5c5455c48D93317139ADDaC8fb;
+    address public sanctionList ; 
 
     // Constructor arguments:
     // logicLabel = "Chainalysis SanctionList"
     // isAllowlistBool = false
     constructor() MoleculeLogic("Chainalysis SanctionList", false) {}
+
+     function setSanctionListAddress(address _sanctionList) external onlyOwner() {
+        sanctionList = _sanctionList;
+    }
 
     function check(address account) external view override returns (bool) {
         return SanctionsList(sanctionList).isSanctioned(account);
